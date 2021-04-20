0c71c7d8d6faadb9e5106c40ef6b90925a061842
filipecosta90 <filipecosta.90@gmail.com>
2021-04-20 17:28:57 +0100

    [fix] Using public AMI of benchmark client

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,7 +13,7 @@ variable "setup_name" {
 
 variable "instance_ami" {
   description = "AMI for aws EC2 instance - us-east-2	focal	20.04 LTS	arm64	hvm:ebs-ssd	20210325"
-  default     = "ami-06e64809872e814b0"
+  default     = "ami-0d08090d8aaa341cd"
 }
 
 variable "grafana_ami" {
